1a708932759e5d58cdaeec5c4eb4dad0c91b8b1b
UmichSamMath <100611214+UmichSamMath@users.noreply.github.com>
2023-10-01 17:26:54 +0000

    weekly edits

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -30,7 +30,7 @@ let SAMevents = [
   {
     "Title": "DW Simpson Info Session",
     "Description": "Come learn more about DW Simpson! RSVP <a target=_blank href=\"https://forms.gle/BYocUKsK2DrM41Zy6\">here</a>.",
-    "Location": "",
+    "Location": "Virtual",
     "Date": "October 3",
     "StartTimeStr": "5:00",
     "EndTimeStr": "6:00",
@@ -41,7 +41,7 @@ let SAMevents = [
   },
   {
     "Title": "Milliman Super Session",
-    "Description": "",
+    "Description": "Learn more in <a target =_blank href = \"/Resources/MillimanFlyerF23.pdf\">this flyer</a>!",
     "Location": "Virtual",
     "Date": "October 4",
     "StartTimeStr": "4:00",
@@ -218,90 +218,6 @@ let SAMevents = [
     "DayofWeek": "Friday",
     "StartTime": "2023-10-27T21:00",
     "EndTime": "2023-10-28T00:00"
-  },
-  {
-    "Title": "Nationwide Info Session",
-    "Description": "Come learn more about Nationwide! Join the call <a target=_blank target = _blank target=_blank target=_blank href=\"https://umich.zoom.us/j/99000537052\">here</a>. RSVP <a target=_blank target = _blank target=_blank target=_blank href=\"https://forms.gle/SR4GwCK7mHVQ9nQg8\">here</a>!",
-    "Location": "Virtual",
-    "Date": "October 4",
-    "StartTimeStr": "5:00",
-    "EndTimeStr": "6:00",
-    "AmPm": "PM",
-    "DayofWeek": "Wednesday",
-    "StartTime": "2023-10-04T17:00",
-    "EndTime": "2023-10-04T18:00"
-  },
-  {
-    "Title": "CAS Intro Workshop",
-    "Description": "",
-    "Location": "East Hall 4096",
-    "Date": "October 5",
-    "StartTimeStr": "6:30",
-    "EndTimeStr": "8:00",
-    "AmPm": "PM",
-    "DayofWeek": "Thursday",
-    "StartTime": "2023-10-05T18:30",
-    "EndTime": "2023-10-05T20:00"
-  },
-  {
-    "Title": "Mentorship Kickoff Workshop",
-    "Description": "MANDATORY for all mentors! Please RSVP via the Speed Dating RSVP.",
-    "Location": "",
-    "Date": "October 23",
-    "StartTimeStr": "6:00",
-    "EndTimeStr": "6:30",
-    "AmPm": "PM",
-    "DayofWeek": "Monday",
-    "StartTime": "2023-10-23T18:00",
-    "EndTime": "2023-10-23T18:30"
-  },
-  {
-    "Title": "Mentorship Speed Dating",
-    "Description": "MANDATORY for everyone in mentorship! RSVP <a target=_blank target = _blank target=_blank target=_blank href=\"https://forms.gle/U6iXmtiwit79fsr19\">here</a>!",
-    "Location": "",
-    "Date": "October 23",
-    "StartTimeStr": "6:30",
-    "EndTimeStr": "8:00",
-    "AmPm": "PM",
-    "DayofWeek": "Monday",
-    "StartTime": "2023-10-23T18:30",
-    "EndTime": "2023-10-23T20:00"
-  },
-  {
-    "Title": "Post Career Fair Recruiting Workshop",
-    "Description": "",
-    "Location": "East Hall 3088",
-    "Date": "October 24",
-    "StartTimeStr": "7:00",
-    "EndTimeStr": "8:30",
-    "AmPm": "PM",
-    "DayofWeek": "Tuesday",
-    "StartTime": "2023-10-24T19:00",
-    "EndTime": "2023-10-24T20:30"
-  },
-  {
-    "Title": "Mentorship RAGER",
-    "Description": "HIGHLY encouraged for everyone in mentorship. Come hang out with all of the mentors and mentees to meet new people and learn who you may want to be partnered with this year! Please RSVP <a target=_blank target = _blank target=_blank target=_blank href=\"https://forms.gle/7dtdJjnSNbdfCtPM6\">here</a>!",
-    "Location": "",
-    "Date": "October 27",
-    "StartTimeStr": "9:00",
-    "EndTimeStr": "12:00",
-    "AmPm": "AM",
-    "DayofWeek": "Friday",
-    "StartTime": "2023-10-27T21:00",
-    "EndTime": "2023-10-28T00:00"
-  },
-  {
-    "Title": "Post Career Fair Recruiting Workshop",
-    "Description": "",
-    "Location": "East Hall 3088",
-    "Date": "October 24",
-    "StartTimeStr": "7:00",
-    "EndTimeStr": "8:30",
-    "AmPm": "PM",
-    "DayofWeek": "Tuesday",
-    "StartTime": "2023-10-24T19:00",
-    "EndTime": "2023-10-24T20:30"
   }
 ]
 

7c61a0aba90ff056a423ed2cd2e96f573005b696
UmichSamMath <100611214+UmichSamMath@users.noreply.github.com>
2023-10-29 17:17:08 +0000

    Updated calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -3,54 +3,44 @@
 // Then the spreadsheet needs to be exported as a csv file, then converted from csv to JSON
 
 let SAMevents = [
-  {
-    "Title": "Mentorship Kickoff Workshop",
-    "Description": "MANDATORY for all mentors! Please RSVP via the Speed Dating RSVP.",
-    "Location": "",
-    "Date": "October 23",
-    "StartTimeStr": "6:00",
-    "EndTimeStr": "6:30",
-    "AmPm": "PM",
-    "DayofWeek": "Monday",
-    "StartTime": "2023-10-23T18:00",
-    "EndTime": "2023-10-23T18:30"
-  },
-  {
-    "Title": "Mentorship Speed Dating",
-    "Description": "MANDATORY for everyone in mentorship! RSVP <a target=_blank href=\"https://forms.gle/U6iXmtiwit79fsr19\">here</a>!",
-    "Location": "",
-    "Date": "October 23",
-    "StartTimeStr": "6:30",
-    "EndTimeStr": "8:00",
-    "AmPm": "PM",
-    "DayofWeek": "Monday",
-    "StartTime": "2023-10-23T18:30",
-    "EndTime": "2023-10-23T20:00"
-  },
-  {
-    "Title": "Post Career Fair Recruiting Workshop",
-    "Description": "",
-    "Location": "East Hall 3088",
-    "Date": "October 24",
-    "StartTimeStr": "7:00",
-    "EndTimeStr": "8:30",
-    "AmPm": "PM",
-    "DayofWeek": "Tuesday",
-    "StartTime": "2023-10-24T19:00",
-    "EndTime": "2023-10-24T20:30"
-  },
-  {
-    "Title": "Mentorship RAGER",
-    "Description": "HIGHLY encouraged for everyone in mentorship. Come hang out with all of the mentors and mentees to meet new people and learn who you may want to be partnered with this year! Please RSVP <a target=_blank href=\"https://forms.gle/7dtdJjnSNbdfCtPM6\">here</a>!",
-    "Location": "",
-    "Date": "October 27",
-    "StartTimeStr": "9:00",
-    "EndTimeStr": "12:00",
-    "AmPm": "AM",
-    "DayofWeek": "Friday",
-    "StartTime": "2023-10-27T21:00",
-    "EndTime": "2023-10-28T00:00"
-  }
+  [
+    {
+      "Title": "Mass Advising Session",
+      "Description": "This session will be led by Professor Natarajan!",
+      "Location": "East Hall B844",
+      "Date": "November 7",
+      "StartTimeStr": "7:30",
+      "EndTimeStr": "9:00",
+      "AmPm": "PM",
+      "DayofWeek": "Tuesday",
+      "StartTime": "2023-11-07T18:30",
+      "EndTime": "2023-11-07T20:00"
+    },
+    {
+      "Title": "Underclassmen Course Planning Workshop",
+      "Description": "Highly encouraged for underclassmen! Plan out your 4 years at college as well as get tips on when to take actuarial exams! Great resources will be provided! Please RSVP <a href=\"https://forms.gle/L3EVHkrwTq72Cdg98\">here</a>.",
+      "Location": "East Hall 4088",
+      "Date": "November 14",
+      "StartTimeStr": "8:00",
+      "EndTimeStr": "9:00",
+      "AmPm": "PM",
+      "DayofWeek": "Tuesday",
+      "StartTime": "2023-11-14T19:00",
+      "EndTime": "2023-11-14T20:00"
+    },
+    {
+      "Title": "DEI Event",
+      "Description": "",
+      "Location": "East Hall 1084",
+      "Date": "November 28",
+      "StartTimeStr": "8:00",
+      "EndTimeStr": "9:00",
+      "AmPm": "PM",
+      "DayofWeek": "Tuesday",
+      "StartTime": "2023-11-28T19:00",
+      "EndTime": "2023-11-28T20:00"
+    }
+  ]
 ]
 
 function populateEvents(){

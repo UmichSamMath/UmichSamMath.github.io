f09aa5ae167eea163ae6b8b8f3ede31358fa0421
UmichSamMath <100611214+UmichSamMath@users.noreply.github.com>
2023-11-27 20:24:24 +0000

    updated calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -3,64 +3,50 @@
 // Then the spreadsheet needs to be exported as a csv file, then converted from csv to JSON
 
 let SAMevents = [
-  [
-    {
-      "Title": "Mass Advising Session",
-      "Description": "This session will be led by Professor Natarajan!",
-      "Location": "East Hall B844",
-      "Date": "November 7",
-      "StartTimeStr": "7:30",
-      "EndTimeStr": "9:00",
-      "AmPm": "PM",
-      "DayofWeek": "Tuesday",
-      "StartTime": "2023-11-07T18:30",
-      "EndTime": "2023-11-07T20:00"
-    },
-    {
-      "Title": "Underclassmen Course Planning Workshop",
-      "Description": "Highly encouraged for underclassmen! Plan out your 4 years at college as well as get tips on when to take actuarial exams! Great resources will be provided! Please RSVP <a href=\"https://forms.gle/L3EVHkrwTq72Cdg98\">here</a>.",
-      "Location": "East Hall 4088",
-      "Date": "November 14",
-      "StartTimeStr": "8:00",
-      "EndTimeStr": "9:00",
-      "AmPm": "PM",
-      "DayofWeek": "Tuesday",
-      "StartTime": "2023-11-14T19:00",
-      "EndTime": "2023-11-14T20:00"
-    },
-    {
-      "Title": "DEI Event",
-      "Description": "",
-      "Location": "East Hall 1084",
-      "Date": "November 28",
-      "StartTimeStr": "8:00",
-      "EndTimeStr": "9:00",
-      "AmPm": "PM",
-      "DayofWeek": "Tuesday",
-      "StartTime": "2023-11-28T19:00",
-      "EndTime": "2023-11-28T20:00"
-    }
-  ]
+  {
+    "Title": "DEI Event",
+    "Description": "SAM will be hosting our first diversity, equity, and inclusion (DEI) event on imposter syndrome! Imposter syndrome is the persistent inability to believe that one's success is deserved or has been legitimately achieved as a result of one's own efforts or skills. We will be talking about methods to overcome imposter syndrome and doing some self reflection! This will be a great destress and reset event before finals, and we highly encourage all SAM members to join! RSVP <a target=_blank href=\"https://forms.gle/C3mSzNctK1YVyRGq7\">here</a>!",
+    "Location": "East Hall 1084",
+    "Date": "November 28",
+    "StartTimeStr": "7:00",
+    "EndTimeStr": "8:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2023-11-28T19:00",
+    "EndTime": "2023-11-28T20:00"
+  },
+  {
+    "Title": "Underclassman Hangout",
+    "Description": "SAM Board is organizing an event for underclassmen to hang out and get to know each other. We will be a hosting murder mystery game night with yummy catered snacks!  RSVP <a target=_blank href=\"https://forms.gle/5v6QHi3HjAaqL7k3A\">here</a>!",
+    "Location": "East Hall 4096",
+    "Date": "November 29",
+    "StartTimeStr": "7:00",
+    "EndTimeStr": "8:00",
+    "AmPm": "PM",
+    "DayofWeek": "Wednesday",
+    "StartTime": "2023-11-29T19:00",
+    "EndTime": "2023-11-29T20:00"
+  }
 ]
 
-function populateEvents(){
+function populateEvents() {
   var resultDiv = document.getElementById("calendar");
   var currDate = new Date(); // gets the current date
-  
+
   SAMevents.forEach(c => {
     var startShowDate = new Date(c.StartTime);
     startShowDate.setDate(startShowDate.getDate() - 7);
-    
+
     var endShowDate = new Date(c.EndTime);
-    if(c.Location === "") {
+    if (c.Location === "") {
       c.Location = "Location To Be Determined";
     }
 
-    if(c.Description === "") {
+    if (c.Description === "") {
       c.Description = "More information coming soon!";
     }
-    
-    if(startShowDate <= currDate && currDate <= endShowDate) {
+
+    if (startShowDate <= currDate && currDate <= endShowDate) {
       resultDiv.innerHTML += `
         <div class="item">
           <p class="event-title">${c.Title}</p>
@@ -72,7 +58,7 @@ function populateEvents(){
           </div>
         </div>
     `;
-    } 
+    }
   });
 }
 

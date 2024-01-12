aee221da7292c00cdc758b64e7ca84b8cc671139
UmichSamMath <100611214+UmichSamMath@users.noreply.github.com>
2024-01-12 16:43:10 +0000

    scholarship, congrats

--- scripts/congrats.js
+++ scripts/congrats.js
@@ -5,51 +5,6 @@ var monthsFeatured = 1;
 
 let congratsMessages = [
   {
-    "name": "Elena Post",
-    "congratsEvent": "passing Exam FM",
-    "dateAdded": new Date(2023, 7, 14)
-  },
-  {
-    "name": "Taylor Siemer",
-    "congratsEvent": "passing Exam P and Exam FM over the summer",
-    "dateAdded": new Date(2023, 7, 27)
-  },
-  {
-    "name": "Sawyer Gustafson",
-    "congratsEvent": "passing Exam SRM",
-    "dateAdded": new Date(2023, 7, 31)
-  },
-  {
-    "name": "Gabe Weber",
-    "congratsEvent": "passing Exam SRM",
-    "dateAdded": new Date(2023, 7, 31)
-  },
-  {
-    "name": "Nicky Li",
-    "congratsEvent": "passing Exam P and Exam FM over the summer",
-    "dateAdded": new Date(2023, 8, 8)
-  },
-  {
-    "name": "Abby Hess",
-    "congratsEvent": "accepting a full-time position as an Actuarial Analyst at Thrivent Financial for June 2024",
-    "dateAdded": new Date(2023, 8, 15)
-  },
-  {
-    "name": "Emily Lin",
-    "congratsEvent": "accepting an internship at State Farm",
-    "dateAdded": new Date(2023, 8, 27)
-  },
-  {
-    "name": "Elena Post",
-    "congratsEvent": "accepting an internship at Milliman",
-    "dateAdded": new Date(2023, 9, 4)
-  },
-  {
-    "name": "Katherine Bednarz",
-    "congratsEvent": "accepting an internship at Deloitte",
-    "dateAdded": new Date(2023, 9, 29)
-  },
-  {
     "name": "Sawyer Gustafson",
     "congratsEvent": "passing exam PA",
     "dateAdded": new Date(2023, 11, 26)
@@ -63,6 +18,11 @@ let congratsMessages = [
     "name": "Michael Williams",
     "congratsEvent": "passing exam FM",
     "dateAdded": new Date(2023, 11, 26)
+  },
+  {
+    "name": "Abby Hess",
+    "congratsEvent": "passing exam ALTAM",
+    "dateAdded": new Date(2024, 0, 12)
   }
 ]
 

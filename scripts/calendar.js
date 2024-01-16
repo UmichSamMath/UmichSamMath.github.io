1db3c43e77d1e358e8e5e9c9b5d9f536688a8fab
UmichSamMath <100611214+UmichSamMath@users.noreply.github.com>
2024-01-16 23:58:41 +0000

    calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -5,7 +5,7 @@
 let SAMevents = [
   {
     "Title": "Winter Mass Meeting",
-    "Description": "RSVP <a href=\"https://forms.gle/7gZMLsF5SCuSeDtz5\">here</a>!",
+    "Description": "RSVP <a target = _blank href=\"https://forms.gle/7gZMLsF5SCuSeDtz5\">here</a>!",
     "Location": "East Hall 1360",
     "Date": "January 16",
     "StartTimeStr": "6:30",
@@ -17,8 +17,8 @@ let SAMevents = [
   },
   {
     "Title": "Speaker Series Presentation - Charlie Johnson",
-    "Description": "Charlie Johnson is a credentialled actuary who began his career at State Farm and later worked at Northwestern Mutual. He has since left the corporate world and started his own company - the Actuarial Development Institute. For more information on ADI and Charlie, visit <a target = _blank href=\"https://www.actuarialdevelopment.com/\">https://www.actuarialdevelopment.com/</a>.  Charlie will be presenting on applying actuarial skills outside of your typical actuarial role. This is a great way to learn more about how useful your actuarial degree is and get ideas about other potential opportunities for you.",
-    "Location": "",
+    "Description": "Charlie Johnson is a credentialled actuary who began his career at State Farm and later worked at Northwestern Mutual. He has since left the corporate world and started his own company - the Actuarial Development Institute. For more information on ADI and Charlie, visit <a target = _blank href=\"https://www.actuarialdevelopment.com/\">https://www.actuarialdevelopment.com/</a>.  Charlie will be presenting on applying actuarial skills outside of your typical actuarial role. This is a great way to learn more about how useful your actuarial degree is and get ideas about other potential opportunities for you. RSVP <a target = _blank href=\"https://forms.gle/z7Rh4Ma6WxPyWjRUA\">here</a>!",
+    "Location": "East Hall 2866",
     "Date": "January 23",
     "StartTimeStr": "6:30",
     "EndTimeStr": "8:00",
@@ -29,7 +29,7 @@ let SAMevents = [
   },
   {
     "Title": "Ross Minor Workshop",
-    "Description": "RSVP <a href=\"https://forms.gle/Y28HKDiyuCSk9sEW7\">here</a>!",
+    "Description": "RSVP <a target = _blank href=\"https://forms.gle/Y28HKDiyuCSk9sEW7\">here</a>!",
     "Location": "Ross R1240",
     "Date": "January 24",
     "StartTimeStr": "6:00",
@@ -41,8 +41,8 @@ let SAMevents = [
   },
   {
     "Title": "Speaker Series Presentation - Milliman",
-    "Description": "Milliman is an actuarial consulting firm. A couple actuaries on the recruiting team from the Chicago office will be coming to present about the differences between consulting and industry. This is a decision that many students struggle with, and this presentation will help provide some knowledge to inform that difficult decision. Pizza will also be provided!",
-    "Location": "",
+    "Description": "Milliman is an actuarial consulting firm. A couple actuaries on the recruiting team from the Chicago office will be coming to present about the differences between consulting and industry. This is a decision that many students struggle with, and this presentation will help provide some knowledge to inform that difficult decision. Pizza will also be provided! RSVP <a target = _blank href=\"https://forms.gle/KNKRq74CpwjnhMEG6\">here</a>!",
+    "Location": "East Hall 2866",
     "Date": "January 31",
     "StartTimeStr": "6:30",
     "EndTimeStr": "8:00",
@@ -53,8 +53,8 @@ let SAMevents = [
   },
   {
     "Title": "Speaker Series Presentation - Northwestern Mutual",
-    "Description": "Northwestern Mutual is a mutual life insurance company based in Milwaukee, Wisconsin. Some actuaries from the recruiting team will be in Ann Arbor to present on Annuities. Annuities are a very interesting insurance product, and there are so many intricacies to learn. This presentation is a great way to expand your knowledge on annuities. Pizza will also be provided!",
-    "Location": "",
+    "Description": "Northwestern Mutual is a mutual life insurance company based in Milwaukee, Wisconsin. Some actuaries from the recruiting team will be in Ann Arbor to present on Annuities. Annuities are a very interesting insurance product, and there are so many intricacies to learn. This presentation is a great way to expand your knowledge on annuities. Pizza will also be provided! RSVP <a target = _blank href=\"https://forms.gle/J4DaCLSyfgdWNNmQA\">here</a>!",
+    "Location": "East Hall 2866",
     "Date": "February 1",
     "StartTimeStr": "6:30",
     "EndTimeStr": "8:00",
@@ -64,9 +64,21 @@ let SAMevents = [
     "EndTime": "2024-02-01T20:00"
   },
   {
-    "Title": "Speaker Series Presentation - Prof. Kausch",
-    "Description": "Professor Kausch will be giving a presentation on “The Actuarial Profession”. Prof Kausch earned his Ph.D. in Mathematics from the University of Michigan in 1993. He is a Fellow of the Society of Actuaries (FSA) and the Conference of Consulting Actuaries (FCA), a Member of the American Academy of Actuaries (MAAA), and an Enrolled Actuary (EA). He served as the Chief Actuary for Gabriel, Roeder, Smith & Company from 2006 until his retirement in 2022. Gabriel, Roeder Smith & Company is the largest actuarial consulting firm dedicated to the public sector. Dr. Kausch taught here between 2002 and 2011 and has since returned to teaching here. He hopes to share knowledge on the actuarial profession with us students.",
+    "Title": "Speaker Series Presentation - Molly Li",
+    "Description": "RSVP <a target = _blank href=\"https://forms.gle/ejE9Vy76aANyhN5S7\">here</a>!",
     "Location": "",
+    "Date": "February 6",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:30",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2024-02-06T18:00",
+    "EndTime": "2024-02-06T19:30"
+  },
+  {
+    "Title": "Speaker Series Presentation - Prof. Kausch",
+    "Description": "Professor Kausch will be giving a presentation on “The Actuarial Profession”. Prof Kausch earned his Ph.D. in Mathematics from the University of Michigan in 1993. He is a Fellow of the Society of Actuaries (FSA) and the Conference of Consulting Actuaries (FCA), a Member of the American Academy of Actuaries (MAAA), and an Enrolled Actuary (EA). He served as the Chief Actuary for Gabriel, Roeder, Smith &amp; Company from 2006 until his retirement in 2022. Gabriel, Roeder Smith &amp; Company is the largest actuarial consulting firm dedicated to the public sector. Dr. Kausch taught here between 2002 and 2011 and has since returned to teaching here. He hopes to share knowledge on the actuarial profession with us students. RSVP <a target = _blank href=\"https://forms.gle/kihos34fPVUJTwS96\">here</a>!",
+    "Location": "East Hall 2866",
     "Date": "February 15",
     "StartTimeStr": "6:30",
     "EndTimeStr": "8:00",

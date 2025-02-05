068aaedd6ae8b2ad776156bb9bdd596b360fa1e3
UmichSamMath <100611214+UmichSamMath@users.noreply.github.com>
2025-02-05 01:23:55 +0000

    calendar update

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -4,42 +4,6 @@
 
 let SAMevents = [
   {
-    "Title": "Winter Welcome ft. Prof. Natarajan",
-    "Description": "RSVP <a target=_blank href=\"https://forms.gle/c3fvD6MZvYFig1Lv8\">here</a>!",
-    "Location": "East Hall 4096",
-    "Date": "January 28",
-    "StartTimeStr": "7:00",
-    "EndTimeStr": "8:00",
-    "AmPm": "PM",
-    "DayofWeek": "Tuesday",
-    "StartTime": "2025-01-28T19:00",
-    "EndTime": "2025-01-28T20:00"
-  },
-  {
-    "Title": "Mentorship Euchre Tournament",
-    "Description": "RSVP <a target=_blank href=\"https://forms.gle/BHepfrThxM7xXbVH9\">here</a>!",
-    "Location": "Mason Hall 1437",
-    "Date": "January 29",
-    "StartTimeStr": "7:00",
-    "EndTimeStr": "9:00",
-    "AmPm": "PM",
-    "DayofWeek": "Wednesday",
-    "StartTime": "2025-01-29T19:00",
-    "EndTime": "2025-01-29T21:00"
-  },
-  {
-    "Title": "Ice Skating @ Yost",
-    "Description": "",
-    "Location": "Yost Ice Arena",
-    "Date": "February 2",
-    "StartTimeStr": "1:30",
-    "EndTimeStr": "3:00",
-    "AmPm": "PM",
-    "DayofWeek": "Sunday",
-    "StartTime": "2025-02-02T13:30",
-    "EndTime": "2025-02-02T15:00"
-  },
-  {
     "Title": "Business Minor Info Session",
     "Description": "",
     "Location": "Ross 1240",
@@ -64,8 +28,20 @@ let SAMevents = [
     "EndTime": "2025-02-06T19:00"
   },
   {
-    "Title": "Mentorship Trivia Night",
+    "Title": "Excel Workshop",
     "Description": "",
+    "Location": "",
+    "Date": "February 11",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2025-02-11T18:00",
+    "EndTime": "2025-02-11T19:00"
+  },
+  {
+    "Title": "Mentorship Trivia Night",
+    "Description": "RSVP <a target=_blank href=\"https://forms.gle/WZ6hSQgdcwm2sJaBA\">here</a>!",
     "Location": "East Hall 1068",
     "Date": "February 13",
     "StartTimeStr": "7:00",
@@ -74,6 +50,18 @@ let SAMevents = [
     "DayofWeek": "Thursday",
     "StartTime": "2025-02-13T19:00",
     "EndTime": "2025-02-13T20:30"
+  },
+  {
+    "Title": "Advanced Excel & VBA Workshop",
+    "Description": "",
+    "Location": "",
+    "Date": "February 19",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Wednesday",
+    "StartTime": "2025-02-19T18:00",
+    "EndTime": "2025-02-19T19:00"
   },
   {
     "Title": "Speaker Series - Actuarial Development Program",
@@ -88,9 +76,21 @@ let SAMevents = [
     "EndTime": "2025-02-20T19:00"
   },
   {
+    "Title": "R Workshop",
+    "Description": "",
+    "Location": "",
+    "Date": "February 25",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2025-02-25T18:00",
+    "EndTime": "2025-02-25T19:00"
+  },
+  {
     "Title": "Speaker Series - Jackson",
     "Description": "Topic: Interviewing 101\nPizza will be provided!",
-    "Location": "",
+    "Location": "East Hall 4088",
     "Date": "February 27",
     "StartTimeStr": "6:00",
     "EndTimeStr": "7:00",
@@ -102,7 +102,7 @@ let SAMevents = [
   {
     "Title": "Northwestern Mutual Info Session",
     "Description": "",
-    "Location": "",
+    "Location": "East Hall 4088",
     "Date": "March 11",
     "StartTimeStr": "6:00",
     "EndTimeStr": "7:00",
@@ -124,9 +124,57 @@ let SAMevents = [
     "EndTime": "2025-03-12T21:30"
   },
   {
+    "Title": "Cigna Speaker Series",
+    "Description": "",
+    "Location": "",
+    "Date": "March 13",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Thursday",
+    "StartTime": "2025-03-13T18:00",
+    "EndTime": "2025-03-13T19:00"
+  },
+  {
+    "Title": "Python Workshop",
+    "Description": "",
+    "Location": "",
+    "Date": "March 20",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Thursday",
+    "StartTime": "2025-03-20T18:00",
+    "EndTime": "2025-03-20T19:00"
+  },
+  {
+    "Title": "SQL Workshop",
+    "Description": "",
+    "Location": "",
+    "Date": "March 25",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2025-03-25T18:00",
+    "EndTime": "2025-03-25T19:00"
+  },
+  {
+    "Title": "Milliman Networking Event",
+    "Description": "",
+    "Location": "",
+    "Date": "March 27",
+    "StartTimeStr": "7:00",
+    "EndTimeStr": "8:00",
+    "AmPm": "PM",
+    "DayofWeek": "Thursday",
+    "StartTime": "2025-03-27T19:00",
+    "EndTime": "2025-03-27T20:00"
+  },
+  {
     "Title": "ial Development Institute Workshop",
     "Description": "Coaching Actuaries partners with ADI for exclusive workshops. This presentation dives into various asset classes, their cash flow structures, and their impact on the financial landscape. From Risk-Free bonds to Venture Capital, you'll explore diverse investment options.",
-    "Location": "",
+    "Location": "Virtual",
     "Date": "March 31",
     "StartTimeStr": "7:00",
     "EndTimeStr": "8:00",
@@ -136,21 +184,33 @@ let SAMevents = [
     "EndTime": "2025-03-31T20:00"
   },
   {
+    "Title": "Communications Workshop",
+    "Description": "",
+    "Location": "",
+    "Date": "April 1",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2025-04-01T18:00",
+    "EndTime": "2025-04-01T19:00"
+  },
+  {
     "Title": "Mentorship Field Day",
     "Description": "",
     "Location": "",
-    "Date": "April 5",
+    "Date": "April 6",
     "StartTimeStr": "12:00",
     "EndTimeStr": "2:00",
     "AmPm": "PM",
-    "DayofWeek": "Saturday",
-    "StartTime": "2025-04-05T12:00",
-    "EndTime": "2025-04-05T14:00"
+    "DayofWeek": "Sunday",
+    "StartTime": "2025-04-06T12:00",
+    "EndTime": "2025-04-06T14:00"
   },
   {
     "Title": "CA Workshop: Actuarial Intelligence That is Not Artificial",
     "Description": "Attend a workshop from the Coaching Actuaries team featuring Coach K and Coach Tong. They will present relevant industry information, share professional growth insights, discuss study tips and answer questions.",
-    "Location": "",
+    "Location": "Virtual",
     "Date": "April 6",
     "StartTimeStr": "4:00",
     "EndTimeStr": "5:00",

8506d28bf88344a1fb571c4c515a0c53d3931525
sjanae-1 <144617298+sjanae-1@users.noreply.github.com>
2026-01-11 17:58:15 -0500

    Update calendar.js

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -4,28 +4,100 @@
 
 let SAMevents = [
   {
-    "Title": "FM Workshop",
-    "Description": "Join us for an FM problem-solving session to prepare for December sittings of the exam! We’ll work through key concepts, problem-solving strategy, and more.<br><br><a href=\"https://umich.us20.list-manage.com/track/click?u=60f792596b0f2eaf62728bf12&amp;id=b31d4feffb&amp;e=63c96118a3\" target=\"_blank\"><u>RSVP HERE</u></a>",
-    "Location": "MH 1427",
-    "Date": "December 2",
-    "StartTimeStr": "6:00",
-    "EndTimeStr": "8:00",
+    "Title": "Winterfest 1",
+    "Description": "",
+    "Location": "Michigan Union, 530 S State St, Ann Arbor, MI 48109, USA",
+    "Date": "January 12",
+    "StartTimeStr": "4:00",
+    "EndTimeStr": "7:00",
     "AmPm": "PM",
-    "DayofWeek": "Tuesday",
-    "StartTime": "2025-12-02T18:00",
-    "EndTime": "2025-12-02T20:00"
+    "DayofWeek": "Monday",
+    "StartTime": "2026-01-12T16:00",
+    "EndTime": "2026-01-12T19:00"
   },
   {
-    "Title": "Mentorship Cookie Decorating",
-    "Description": "Join us for a fun way to connect with your mentorship pairing! We will decorate sugar cookies while watching a holiday movie!<br><br><a href=\"https://umich.us20.list-manage.com/track/click?u=60f792596b0f2eaf62728bf12&amp;id=a48c6387c2&amp;e=63c96118a3\" target=\"_blank\"><u>RSVP HERE</u></a>",
-    "Location": "EH 4096",
-    "Date": "December 3",
+    "Title": "Winterfest 2",
+    "Description": "",
+    "Location": "Michigan Union, 530 S State St, Ann Arbor, MI 48109, USA",
+    "Date": "January 13",
+    "StartTimeStr": "4:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2026-01-13T16:00",
+    "EndTime": "2026-01-13T19:00"
+  },
+  {
+    "Title": "SAM Project Team Introduction",
+    "Description": "Join us for the introductory meeting of SAM's first ever project team! This is a great opportunity to bolster your resume and strengthen your technical skills in a collaborative learning environment. Work alongside other members on a semester-long project to build real-world experience.",
+    "Location": "EH 1866",
+    "Date": "January 14",
     "StartTimeStr": "6:00",
     "EndTimeStr": "7:00",
     "AmPm": "PM",
     "DayofWeek": "Wednesday",
-    "StartTime": "2025-12-03T18:00",
-    "EndTime": "2025-12-03T19:00"
+    "StartTime": "2026-01-14T18:00",
+    "EndTime": "2026-01-14T19:00"
+  },
+  {
+    "Title": "Winter Mass Meeting",
+    "Description": "Attend to learn all about SAM, the actuarial profession, and our upcoming winter semester events!",
+    "Location": "EH 1086",
+    "Date": "January 20",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "8:00",
+    "AmPm": "PM",
+    "DayofWeek": "Tuesday",
+    "StartTime": "2026-01-20T18:00",
+    "EndTime": "2026-01-20T20:00"
+  },
+  {
+    "Title": "Winter Welcome",
+    "Description": "Join us to meet other members and learn more about the resources SAM provides. Underclassmen are highly encouraged to attend!",
+    "Location": "NUB 1518",
+    "Date": "January 21",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "8:00",
+    "AmPm": "PM",
+    "DayofWeek": "Wednesday",
+    "StartTime": "2026-01-21T18:00",
+    "EndTime": "2026-01-21T20:00"
+  },
+  {
+    "Title": "Yost Ice Skating",
+    "Description": "Meet up with SAM members for a fun afternoon of open skate at Yost! The cost is $5 for entry and $5 to rent skates. This is a great event to attend with your mentor/mentee!",
+    "Location": "Yost Ice Arena, 1116 S State St, Ann Arbor, MI 48109, USA",
+    "Date": "January 25",
+    "StartTimeStr": "1:30",
+    "EndTimeStr": "3:30",
+    "AmPm": "PM",
+    "DayofWeek": "Sunday",
+    "StartTime": "2026-01-25T13:30",
+    "EndTime": "2026-01-25T15:30"
+  },
+  {
+    "Title": "Milliman Speaker Series",
+    "Description": "Milliman will be coming to present about Medicare Advantage. Attend to learn more about this healthcare product and its relevance in the industry.<br><br><a href=\"https://umich.us20.list-manage.com/track/click?u=60f792596b0f2eaf62728bf12&amp;id=3bdbe58a35&amp;e=63c96118a3\" target=\"_blank\"><u>RSVP HERE</u></a>",
+    "Location": "AH G168",
+    "Date": "January 28",
+    "StartTimeStr": "6:00",
+    "EndTimeStr": "7:00",
+    "AmPm": "PM",
+    "DayofWeek": "Wednesday",
+    "StartTime": "2026-01-28T18:00",
+    "EndTime": "2026-01-28T19:00"
+  },
+  {
+    "Title": "Mentorship Euchre Night",
+    "Description": "Pair up with your mentor/mentee and compete in our annual mentorship Euchre tournament!",
+    "Location": "Weiser 110",
+    "Date": "January 29",
+    "StartTimeStr": "7:00",
+    "EndTimeStr": "9:00",
+    "AmPm": "PM",
+    "DayofWeek": "Thursday",
+    "StartTime": "2026-01-29T19:00",
+    "EndTime": "2026-01-29T21:00"
   }
 ];
 

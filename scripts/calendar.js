1fce254a66524be562941ec505a8cb932ad4847d
sjanae-1 <144617298+sjanae-1@users.noreply.github.com>
2026-04-05 12:22:58 -0400

    Update event descriptions and locations in calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -4,56 +4,8 @@
 
 let SAMevents = [
   {
-    "Title": "Board Applications Due",
-    "Description": "Board elections are coming up! If you're running for a position, be sure to submit the application by <strong>midnight</strong> on Monday.<br><a href=\"https://umich.us20.list-manage.com/track/click?u=60f792596b0f2eaf62728bf12&amp;id=2025932c0d&amp;e=63c96118a3\" target=\"_blank\"><u>Apply Here</u></a>",
-    "Location": "",
-    "Date": "March 30",
-    "StartTimeStr": "12:00",
-    "EndTimeStr": "12:00",
-    "AmPm": "AM",
-    "DayofWeek": "Monday",
-    "StartTime": "2026-03-30T00:00",
-    "EndTime": "2026-03-31T00:00"
-  },
-  {
-    "Title": "Intro to the CAS Workshop / Internship Panel",
-    "Description": "Are you interested in the CAS or unsure about which path you want to go down? Come to this workshop to learn more about property & casualty insurance from a panel of students with internship experience!",
-    "Location": "EH 3096",
-    "Date": "March 31",
-    "StartTimeStr": "7:00",
-    "EndTimeStr": "8:00",
-    "AmPm": "PM",
-    "DayofWeek": "Tuesday",
-    "StartTime": "2026-03-31T19:00",
-    "EndTime": "2026-03-31T20:00"
-  },
-  {
-    "Title": "Board Elections Interviews",
-    "Description": "If you're running for board, each candidate must meet with the current board member in the position they are applying for to discuss the role and expectations.",
-    "Location": "Math Atrium",
-    "Date": "April 1",
-    "StartTimeStr": "12:00",
-    "EndTimeStr": "12:00",
-    "AmPm": "AM",
-    "DayofWeek": "Wednesday",
-    "StartTime": "2026-04-01T00:00",
-    "EndTime": "2026-04-04T00:00"
-  },
-  {
-    "Title": "Everything Exams Workshop + CA Event",
-    "Description": "This workshop will cover the different exam paths and everything you need to know about the actuarial exams! Come if you want to learn more about the resources that will help you succeed!",
-    "Location": "EH 4096",
-    "Date": "April 1",
-    "StartTimeStr": "6:00",
-    "EndTimeStr": "7:00",
-    "AmPm": "PM",
-    "DayofWeek": "Wednesday",
-    "StartTime": "2026-04-01T18:00",
-    "EndTime": "2026-04-01T19:00"
-  },
-  {
     "Title": "SAM Board Elections",
-    "Description": "",
+    "Description": "All membership is encouraged to attend to elect SAM's new executive board. Food provided!",
     "Location": "EH 1324",
     "Date": "April 8",
     "StartTimeStr": "6:00",
@@ -65,8 +17,8 @@ let SAMevents = [
   },
   {
     "Title": "Mentorship Field Day",
-    "Description": "",
-    "Location": "",
+    "Description": "Get some fresh air and hang out with other SAM members! There will be water balloons and other fun games!",
+    "Location": "Mitchell Field",
     "Date": "April 11",
     "StartTimeStr": "11:00",
     "EndTimeStr": "2:00",
